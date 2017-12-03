35b8da70e1093410c359ac09bda2ba9d220e0a9e
Giancarlo <giancalonghi@hotmail.com>
2017-12-03 09:16:31 +0000

    Lo de facebook, Posts(sin edicion), pagina principal, signout y no creo que nadamas(por si acaso)

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,7 +1,7 @@
 class MainController < ApplicationController
     def main
         @images = Image.where(in_front_page: true).order(created_at: :desc).limit(4);
-        @posts = Post.order(date: :desc).limit(2);
+        @posts = Post.order(date: :desc).limit(3);
     end
     
     def about
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,63 +1,49 @@
 class PostsController < ApplicationController
-    #before_action :find_post, only: [:edit, :update, :show, :delete]
-        @titulo
-        @descripcion
-        @cuerpo
-        @imagen
-        @image
-        @nombreimagen = ""
-        @descripimagen = ""
-        #@currentUser = User.first
-
-        
+    before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+        @@etiqueta  = ["Arte", "Jugabilidad"]
+        @@categoria = ["MOBA","Shooter"]
         def index
             @posts = Post.page(params[:page]).per(3)
-            #@posts = Post.limit(1*@numPag).order(desc).limit(1)
-            #@posts = Post.listing(params[:page])            
-            #@posts = Post.listing(params[:page]).per(1).order(created_at: :desc)
-            #@titulo = "lol"
-            #@users = User.all
-            #Post.count                     #=> 1000
-            #@postss = Post.limit(2); @postss.count     #=> 5
-            #@postss.page(1).per(20).size         #=> 20
-            #@postss.page(1).per(20).total_count
-            #@postss = Post.order(:id).page params[:page]
         end
         
         def new
             @post = Post.new
-            #@user = User.new
-            @etiquetas = Array["Arte", "Jugabilidad"]
-            @categorias = Array["MOBA","Shooter"]
+            @etiquetas = @@etiqueta
+            @categorias = @@categoria
             @nombre = "Crear Post"
-
         end
         
         def create
             @etiqTem = Array.new
             @categTem = Array.new
             @post = Post.new
-            #user = User.where(username: "Dratad")
-            #t = Time.now
             @post[:title] = params[:post][:title]
-            @post[:date] = DateTime.now.strftime('%Y-%m-%d %H:%M:%S')#DateTime.now.to_formatted_s(:db)#.strftime('%Y-%m-%d %H:%M:%S')
+            @post[:date] = DateTime.now.strftime('%Y-%m-%d %H:%M:%S')
             @post[:description] = params[:post][:description]
-            @post[:user_id] = current_user[:id] #user[0][:id] #User.first.
-            if params.include? "Arte"
-                etiqTem.push("Arte")
+            @post[:user_id] = current_user[:id]
+            params.each do |a| 
+                if @@etiqueta.include? a
+                    @etiqTem.push(a)
+                end
+                if @@categoria.include? a
+                    @categTem.push(a)
+                end 
             end
-            if params.include? "MOBA"
-                etiqTem.push("MOBA")
-            end
-            #@game[:information] = params[:game][:information]
-            #@game[:requirements] = params[:game][:requirements]
+            @post[:categories] = @categTem
+            @post[:tags] = @etiqTem
             @post.save
-            #redirect_to "/users/creaTemp" and return
+            flash[:notice] = "Post Creado!"
+            redirect_to action: "index"
         end
         
         def show
             @post = Post.find(params[:id])
         end
-
+        
+        def destroy
+            @post = Post.find(params[:id])
+            @post.destroy
+            redirect_to action: "index"
+        end
     
 end
