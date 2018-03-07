50109e2a6168468dd5f67e7265ee43991b302759
Giancarlo <giancalonghi@hotmail.com>
2018-03-07 22:39:42 +0000

    probando mail

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -5,11 +5,15 @@ class ContactController < ApplicationController
     end
     
     def enviar
-      name = params[:name]
-      email = params[:email]
-      subject = params[:subject]
-      message = params[:message]
-      GuestMailer.enviar(name, email, subject, message)
+      @name = params[:name]
+      @email = params[:email]
+      @subject = params[:subject]
+      @message = params[:message]
+      #GuestMailer.enviar(name, email, subject, message)
+      mail(to: "admin@gameslavesteam.com") do |format|
+        format.html { render layout: 'enviar' }
+        format.text
+      end
       redirect_to action: "contact"  
     end
 end
