8e80352b2293d78bc893fa667097e120f8313e4b
Giancarlo <giancalonghi@hotmail.com>
2018-03-08 01:38:02 +0000

    probando mailx3

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -1,17 +1,28 @@
 class ContactController < ApplicationController
     before_action :authenticate_user!
+    default :from => "admin@gameslavesteam.com"
     def contact
       
     end
     
     def enviar
-      name = params[:name]
-      email = params[:email]
-      subject = params[:subject]
-      message = params[:message]
-      tosend = "admin@gameslavesteam.com"
+      @name = params[:name]
+      @email = params[:email]
+      @subject = params[:subject]
+      @message = params[:message]
+      @tosend = "admin@gameslavesteam.com"
       #GuestMailer.enviar(name, email, subject, message)
-      mail(:to => tosend, :subject => params[:subject], :text => params[:message] )
+      #mail(:to => tosend, :subject => params[:subject], :text => params[:message] )
+      correo(@name, @email, @subject, @message, @tosend).deliver
       redirect_to action: "contact"  
+    end
+    
+    def correo(name, email, subject, message, tosend)
+      @name = name
+      @email = email
+      @subject = subject
+      @message = message
+      @tosend = tosend
+      mail(:to => tosend, :subject => subject, :text => message )
     end
 end
