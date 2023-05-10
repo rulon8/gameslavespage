2404e929e3598760beb35c3acb7ad8006a4d9c76
Giancarlo Longhi <rulon@Giancarlos-MacBook-Pro.local>
2023-05-09 22:07:26 -0600

    Update deployment config

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,7 @@
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
-server '68.66.205.62', user: 'deploy', roles: %w{app db web}, port: 7822
+server '190.92.151.146', user: 'deploy', roles: %w{app db web}, port: 7822
 
 
 # role-based syntax
